af63f65b5665926e0b663ed966213184d4caaf4d
Marcos <marcos.mrpb@gmail.com>
2023-01-11 16:56:18 -0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Olá Mundo!</h1>
+    <h2>Seja bem-vindo</h2>
+</body>
+</html>
